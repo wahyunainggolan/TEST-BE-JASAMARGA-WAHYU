20727e89586bb63b6283daf1da9b525e5136946f
wahyu <wahyuhatake20@gmail.com>
2024-03-15 01:04:11 +0700

    JM-jawaban no 3

---
+++ RAW_QUERY/query_3_employee_education.sql
@@ -0,0 +1,4 @@
+INSERT INTO education (employee_id, name, level, description, created_by, created_at, updated_by, updated_at)
+VALUES
+(1, 'SMKN 7 Jakarta', 'Strata 1', 'Sekolah Menengah Atas', 'admin', '2022-12-12', 'admin', '2022-12-12'),
+(2, 'Universitas Negeri Jakarta', 'Strata 1', 'Sarjana', 'admin', '2022-12-12', 'admin', '2022-12-12');
---
+++ RAW_QUERY/query_3_employee_profile.sql
@@ -0,0 +1,4 @@
+INSERT INTO employee_profile (employee_id, place_of_birth, date_of_birth, gender, is_married, prof_pict, created_by, created_at, updated_by, updated_at)
+VALUES
+(1, 'Jakarta', '1997-05-02', 'Laki-Laki', true, '', 'admin', '2022-12-12', 'admin', '2022-12-12'),
+(2, 'Sukabumi', '1996-05-02', 'Perempuan', false, '', 'admin', '2022-12-12', 'admin', '2022-12-12');
